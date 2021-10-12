ceeaa5070279e6353049f402c70810fb574a9b3c
Cherie Lum <johndoe@example.com>
2021-10-11 21:38:38 -0400

    first push

---
+++ recipes/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  // 1
+@override
+Widget build(BuildContext context) {
+  // 2
+  final ThemeData theme = ThemeData();
+  // 3
+  return MaterialApp(
+    // 4
+    title: 'Recipe Calculator',
+    // 5
+    theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.grey,
+            secondary: Colors.black,
+        ),
+    ),
+    // 6
+    home: const MyHomePage(title: 'Recipe Calculator'),
+  );
+}
+
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Update to return Recipe card
+            return Text(Recipe.samples[index].label);
+  },
+),
+
+
+        ),
+    );
+  }
+
+  // TODO: Add buildRecipeCard() here
+}
+
